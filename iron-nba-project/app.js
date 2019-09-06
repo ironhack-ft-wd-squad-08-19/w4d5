abf5077e4acf6f9453c1a388eae4e1293df28350
mjarraya <mjarraya@student.42.fr>
2019-09-06 17:37:57 +0200

    ironnba

--- iron-nba-project/app.js
+++ iron-nba-project/app.js
@@ -29,7 +29,7 @@ app.get("/teams", (req, res) => {
 });
 
 // prettier-ignore
-const players = [ { name: "Russell", lastName: "Westbrook", team: "OKC", photo: "https://thunderousintentions.com/wp-content/uploads/getty-images/2017/12/891998404-oklahoma-city-thunder-v-indiana-pacers.jpg.jpg" }, { name: "Kevin", lastName: "Durant", team: "GSW", photo: "https://img.bleacherreport.net/img/images/photos/003/670/482/hi-res-3c2473cd8600df96c4b94c93808562c8_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top" }, { name: "Lebron", lastName: "James", team: "CLE", photo: "https://usatftw.files.wordpress.com/2018/01/ap_cavaliers_timberwolves_basketball.jpg?w=1000&h=600&crop=1" }, { name: "Emanuel", lastName: "Ginóbilli", team: "SAS", photo: "https://cdn.vox-cdn.com/thumbor/Z9kR0HDJrzOzxOdwGe7Jwyxxvjk=/0x0:2802x4203/1200x800/filters:focal(1329x1158:1777x1606)/cdn.vox-cdn.com/uploads/chorus_image/image/57733525/usa_today_10429631.0.jpg" }, { name: "Kyrie", lastName: "Irving", team: "BOS", photo: "https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/11/11/kyrie-irving-mvp-case.jpg?itok=PWYqUSGf" }, { name: "Kobe", lastName: "Bryant", team: "LAK", photo: "https://clutchpoints.com/wp-content/uploads/2017/10/Kobe-Bryant-e1508564618882.jpg" } ];
+const players = [ { name: "Russell", lastName: "Westbrook", team: "OKC", photo: "https://thunderousintentions.com/wp-content/uploads/getty-images/2017/12/891998404-oklahoma-city-thunder-v-indiana-pacers.jpg.jpg" }, { name: "Kevin", lastName: "Durant", team: "OKC", photo: "https://img.bleacherreport.net/img/images/photos/003/670/482/hi-res-3c2473cd8600df96c4b94c93808562c8_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top" }, { name: "Lebron", lastName: "James", team: "CLE", photo: "https://usatftw.files.wordpress.com/2018/01/ap_cavaliers_timberwolves_basketball.jpg?w=1000&h=600&crop=1" }, { name: "Emanuel", lastName: "Ginóbilli", team: "SAS", photo: "https://cdn.vox-cdn.com/thumbor/Z9kR0HDJrzOzxOdwGe7Jwyxxvjk=/0x0:2802x4203/1200x800/filters:focal(1329x1158:1777x1606)/cdn.vox-cdn.com/uploads/chorus_image/image/57733525/usa_today_10429631.0.jpg" }, { name: "Kyrie", lastName: "Irving", team: "BOS", photo: "https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/11/11/kyrie-irving-mvp-case.jpg?itok=PWYqUSGf" }, { name: "Kobe", lastName: "Bryant", team: "LAK", photo: "https://clutchpoints.com/wp-content/uploads/2017/10/Kobe-Bryant-e1508564618882.jpg" } ];
 
 app.get("/players", (req, res) => {
   res.render("players.hbs", { playersList: players });
@@ -37,9 +37,32 @@ app.get("/players", (req, res) => {
 
 // app.get('/teams/:teamId/players/:playerId')
 
+//localhost:3000/players/bar
 app.get("/players/:playerName", (req, res) => {
-  //   console.log(req.params.playerName);
-  //   res.send(req.params);
+  // render the correct player from the `players` array
+  //   console.log(req.params);
+
+  const singlePlayer = players.find(el => {
+    if (el.name === req.params.playerName) {
+      return true;
+    }
+  });
+
+  res.render("player.hbs", { player: singlePlayer });
+  //   for (const el of players) {
+  //     if (el.name === req.params.playerName) {
+  //       res.render("player.hbs", { player: singlePlayer });
+  //       return;
+  //     }
+  //   }
+});
+
+app.get("/search", (req, res) => {
+  const playersList = players.filter(el => {
+    if (el.team === req.query.q) return true;
+  });
+
+  res.render("players.hbs", { playersList: playersList });
 });
 
 // app.get("/players/kobe", (req, res) => {
